d6c9f09e9864fba8f6bb8cfde380776e06bff42b
wuqingvika <526288022@qq.com>
2018-10-05 15:39:59 +0800

    3.整合SpringBoot on 201810005

---
+++ boot-order-service-consumer/src/main/java/com/wq/dubbo/controller/OrderController.java
@@ -0,0 +1,25 @@
+package com.wq.dubbo.controller;
+
+import com.wq.dubbo.bean.UserAddress;
+import com.wq.dubbo.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @ResponseBody
+    @RequestMapping("/initOrder")
+    public List<UserAddress> initOrder(@RequestParam("uid")String userId) {
+        return orderService.initOrder(userId);
+    }
+
+}
